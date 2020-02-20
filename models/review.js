6365cb9c1b9d77ac084d747ff2045baf4385f1a1
Chris Stephens <stephens-c2@live.com>
2020-02-19 20:25:25 -0700

    Creating Tables using sequalize

--- models/review.js
+++ models/review.js
@@ -1,7 +1,19 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
     var reviews = sequelize.define('reviews', {
         ID: DataTypes.STRING,
         Stars_ID: DataTypes.STRING,
         roommatesId: DataTypes.STRING
 
-    })}
+    })
+    reviews.associate = function (models) {
+        // We're saying that a Post should belong to an Author
+        // A Post can't be created without an Author due to the foreign key constraint
+        reviews.belongsTo(models.User, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+
+    return Post;
+};
--- models/roommates.js
+++ models/roommates.js
@@ -1,10 +1,21 @@
-module.exports = function(sequelize, DataTypes) {
-    var roommates = sequelize.define('roommmates', {
-        ID:   DataTypes.STRING,
-        firstName: DataTypes.STRING,
-        lastName: DataTypes.STRING,
-        Email: DataTypes.STRING,
-        Gender: DataTypes.STRING,
-        Stars: DataTypes.STRING
-    })}
+module.exports = function (sequelize, DataTypes) {
+  var roommates = sequelize.define('roommmates', {
+    ID: DataTypes.STRING,
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    Email: DataTypes.STRING,
+    Gender: DataTypes.STRING,
+    Stars: DataTypes.STRING
+  })
+  roommates.associate = function (models) {
+    // We're saying that a Post should belong to an Author
+    // A Post can't be created without an Author due to the foreign key constraint
+    roommates.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
 
+  return Post;
+};
