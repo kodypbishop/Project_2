4ae9300eabeb4c74f995b088713c18ab596f7db4
clcamou <clcamou@gmail.com>
2020-02-25 19:45:33 -0700

    working on profile calls

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,15 +1,18 @@
-$(document).ready(function(){
-    $.get("/api/user_data").then(function(data){
-        console.log(data);
-    })
-})
+$(document).ready(function (data) {
+    $("/api/profile", function () {
+        $.ajax({
+            url: "/api/profile",
+            type: "GET",
+            data: {
+                firstName: $("#firstName").val(),
+                lastName: $("#lastName").val(),
+                gender: $("#gender").val(),
+                stars: $("#lastName").val(),
 
-$(document).ready(function() {
-    // This file just does a GET request to figure out which user is logged in
-    // and updates the HTML on the page
-    $.get("/api/user_data").then(function(data) {
-        console.log(data);
-      $(".member-name").text(data.firstName);
-    });
-  
-  });
+            }.then(function () {
+                console.log(data)
+                window.location.replace("/profile");
+            })
+        })
+    })
+});
