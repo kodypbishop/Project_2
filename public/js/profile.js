25973dbfa66f98dfc98b5477ce77362f529c4276
clcamou <clcamou@gmail.com>
2020-02-25 17:37:21 -0700

    fixed color

--- public/js/profile.js
+++ public/js/profile.js
@@ -3,3 +3,13 @@ $(document).ready(function(){
         console.log(data);
     })
 })
+
+$(document).ready(function() {
+    // This file just does a GET request to figure out which user is logged in
+    // and updates the HTML on the page
+    $.get("/api/user_data").then(function(data) {
+        console.log(data);
+      $(".member-name").text(data.firstName);
+    });
+  
+  });
